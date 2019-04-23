a587f0a5efaff26e597fc67edef2dd28f34b563d
haixing.yu <yhx_5178@163.com>
2019-04-23 18:00:39 +0800

    curl调试

--- application/index/controller/Btc.php
+++ application/index/controller/Btc.php
@@ -39,9 +39,18 @@ class Btc
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($curlPost));
-        $data = curl_exec($ch);//运行curl
-        curl_close($ch);
         
-        return $data;
+        $result = curl_exec($ch);//运行curl
+
+        if ($result) {
+            curl_close($ch);
+
+            return json_decode($result, true);
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+
+            return 'curl出错，错误码('.$error.')';
+        }
     }
 }
