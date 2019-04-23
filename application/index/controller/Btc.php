40a434b4b6c684da9f89e57ec710f7c162427da1
haixing.yu <yhx_5178@163.com>
2019-04-23 17:55:32 +0800

    比特币测试调用

--- application/index/controller/Btc.php
+++ application/index/controller/Btc.php
@@ -36,7 +36,7 @@ class Btc
         curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($curlPost));
         $data = curl_exec($ch);//运行curl
         curl_close($ch);
         
